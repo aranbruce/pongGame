75a250a6ab4fde015f3abe81709f0a982ef7d93e
Aran <aranbruce@gmail.com>
2017-12-25 15:28:22 +0000

    added touch event

--- scripts/jsScripts.js
+++ scripts/jsScripts.js
@@ -61,6 +61,40 @@ window.onload = function() {
       var mousePos = calculateMousePos(evt);
       paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
     });
+
+  // tracking touch movement
+  function TouchListener(element) {
+    this.touches = [];
+    this.touchMoveListener = function(touch) {};
+
+    element.addEventListener("touchstart", (function(e) {
+        e.preventDefault();
+        for (var i = 0; i < e.changedTouches.length; i++) {
+            var touch = e.changedTouches[i];
+            this.touches[touch.identifier] = {x: touch.clientX, y: touch.clientY};
+        }
+    }).bind(this));
+
+    element.addEventListener("touchmove", (function(e) {
+        e.preventDefault();
+        for (var i = 0; i < e.changedTouches.length; i++) {
+            var touch = e.changedTouches[i];
+            var previousTouch = this.touches[touch.identifier];
+            this.touches[touch.identifier] = {x: touch.clientX, y: touch.clientY};
+
+            var offset = {x: touch.clientX - previousTouch.x, y: touch.clientY - previousTouch.y}
+            this.touchMoveListener({x: touch.clientX, y: touch.clientY, offset: offset});
+        }
+    }).bind(this));
+
+    element.addEventListener("touchend", (function(e) {
+        e.preventDefault();
+        for (var i = 0; i < e.changedTouches.length; i++) {
+            delete this.touches[e.changedTouches[i].identifier];
+        }
+    }).bind(this));
+  }
+
 }
 
 function computerMovement() {
