33f0b0134b9fa9772436cad3529a91949a1c34e0
Aran <aranbruce@gmail.com>
2017-12-25 00:44:51 +0000

    code cleanup

--- scripts/jsScripts.js
+++ scripts/jsScripts.js
@@ -8,19 +8,24 @@ var ballX = 50;
 window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
-  setInterval(drawEverything, 1000);
+
+  var framesPerSecond = 30;
+  setInterval(function() {
+    moveEverything();
+    drawEverything();
+  }, 1000/framesPerSecond);
 }
 
+function moveEverything() {
+    ballX += 5;
+}
 
 function drawEverything() {
-  ballX += 10;
-
-  console.log(ballX);
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(225,210,200,200);
+  canvasContext.fillRect(0,210,10,100);
   canvasContext.fillStyle = 'red';
-  canvasContext.fillRect(ballX,200,50,25);
+  canvasContext.fillRect(ballX,100,10,10);
 
 }
