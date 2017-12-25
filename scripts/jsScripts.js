1c019eb12ede05ed6e294ab34bf7f4ce3307dc2c
Aran <aranbruce@gmail.com>
2017-12-25 00:09:58 +0000

    added var ballX

--- scripts/jsScripts.js
+++ scripts/jsScripts.js
@@ -5,13 +5,13 @@ var canvasContext;
 
 // Stops script loading before page has finished loading
 window.onload = function() {
-  console.log("Hello World!");
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   drawEverything();
 }
 
 function drawEverything() {
+  console.log("called drawEverything");
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
 }
