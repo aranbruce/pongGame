9e133903790ff7e842ce252985da9f78d47e80a7
Aran <aranbruce@gmail.com>
2017-12-25 00:06:23 +0000

    added canvas

--- scripts/jsScripts.js
+++ scripts/jsScripts.js
@@ -1,3 +1,14 @@
+// Variables
+
+var canvas;
+var canvasContext;
+
+// Stops script loading before page has finished loading
 window.onload = function() {
   console.log("Hello World!");
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+
 }
